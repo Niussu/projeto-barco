3410855681d3ccea545de0ef00f5eed0697d06c9
Vinicius L <81776878+Niussu@users.noreply.github.com>
2023-04-08 14:16:29 -0300

    Add files via upload

--- ProjetoBarco/actions_php/return_all.php
+++ ProjetoBarco/actions_php/return_all.php
@@ -1,8 +1,25 @@
-<?php
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../styles/show.css">
+    <link rel="stylesheet" href="../styles/body.css">
+    <title>Document</title>
+</head>
+<body>
+<button><a href="../pages/index.html">Voltar ao menu</a></button>
+    <div>
+        <h1>Lista dos barcos cadastrados</h1>
+    <?php
     require_once('../models/conect.php');
     $conection = new Conect('localhost', 'db_barco', 'root', '');
     $table = 'barco';
     foreach ($conection->ReturnAll($table) as $key) {
-        echo $key['id']." / ".$key['model']." / ".$key['manufacturer']." / ".$key['optional_description']." / ".$key['color']."<br>";
+        echo "<p>".$key['id']." / ".$key['model']." / ".$key['manufacturer']." / ".$key['optional_description']." / ".$key['color']."</p    ><br>";
     }
-?>
+    ?>
+    </div>
+</body>
+</html>
--- ProjetoBarco/actions_php/search.php
+++ ProjetoBarco/actions_php/search.php
@@ -1,3 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../styles/show.css">
+    <link rel="stylesheet" href="../styles/body.css">
+    <title>Document</title>
+</head>
+<body>
+<button><a href="../pages/index.html">Voltar ao menu</a></button>
+    <div>
+        <h1>Barcos que correspondem a pesquisa</h1>
 <?php
     require_once('../models/conect.php');
     if ($_SERVER['REQUEST_METHOD'] === 'POST'){
@@ -6,7 +20,10 @@
         $sql = "SELECT * FROM `barco` WHERE `model` = ?;";
         $param = array($word);
         foreach ($conection->Consult($sql, $param) as $key) {
-            echo $key['id']." / ".$key['model']." / ".$key['manufacturer']." / ".$key['optional_description']." / ".$key['color'];
+            echo "<p>".$key['id']." / ".$key['model']." / ".$key['manufacturer']." / ".$key['optional_description']." / ".$key['color']."</p>";
         }
     }
 ?>
+    </div>
+</body>
+</html>
